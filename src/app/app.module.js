be352b9fd96fb562dab6b9d3227ee8dc077298f7
1forh <1forhlol@gmail.com>
2017-03-05 18:50:59 -0500

    fixes issue with ga

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -8,7 +8,7 @@ angular
 		'app.common'
 	]).config(function(AnalyticsProvider) {
 		AnalyticsProvider.setAccount('UA-59393507-1');
-	}).run(function($rootScope, $timeout, $location) {
+	}).run(function($rootScope, $timeout, $location, Analytics) {
 		$rootScope.$on('$stateChangeSuccess', function() {
 			document.body.scrollTop = 0;
 			document.documentElement.scrollTop = 0;
