93836f07d77fc734f397539c8b8b9fd95d441e62
1forh <1forhlol@gmail.com>
2017-03-25 11:12:16 -0400

    fixes metadata component and adds titles

--- src/app/components/zm-metadata/zm-metadata.controller.js
+++ src/app/components/zm-metadata/zm-metadata.controller.js
@@ -1,11 +1,13 @@
-function ZMMetadataController($rootScope, $location) {
+function ZMMetadataController($scope, $rootScope, $location, $timeout) {
 	let ctrl = this;
 
-	$rootScope.metadata = {
-		url: $location.absUrl(),
-		title: ctrl.title ? ctrl.title : 'Zach Minner',
-		description: ctrl.description ? ctrl.description : 'Hey! My name is Zach Minner. I am a front-end web developer at SKYCATCHFIRE who lives off Mr. Robot and grilled cheese sandwiches.'
-	};
+	$timeout(function() {
+		$rootScope.metadata = {
+			url: $location.absUrl(),
+			title: ctrl.title ? ctrl.title : 'Zach Minner',
+			description: ctrl.description ? ctrl.description : 'Hey! My name is Zach Minner. I am a front-end web developer at SKYCATCHFIRE who lives off Mr. Robot and grilled cheese sandwiches.'
+		};
+	});
 
 }
 
