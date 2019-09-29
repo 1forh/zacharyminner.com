1032f618bd0ef44f1fcd58c0ca91390c4ace7d26
1forh <1forhlol@gmail.com>
2019-09-29 15:45:58 -0400

    wip

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,7 +13,7 @@ class BlogIndex extends React.Component {
 
     return (
       <Layout location={this.props.location} title={siteTitle}>
-        <SEO title="All posts" />
+        <SEO title="Blog" />
         {posts.map(({ node }) => {
           const title = node.frontmatter.title || node.fields.slug;
           return (
