68b34f2fb971cf69f26b46bcd967f1ab409a77a5
1forh <1forhlol@gmail.com>
2019-09-29 21:47:45 -0400

    wip

--- gatsby-config.js
+++ gatsby-config.js
@@ -51,7 +51,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-59393507-16`,
       },
     },
     `gatsby-plugin-feed`,
