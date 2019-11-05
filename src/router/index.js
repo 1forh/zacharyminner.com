97a0ea12de557f1fcf4b633f1b521ca7823baf39
1forh <1forhlol@gmail.com>
2019-11-05 18:26:05 -0500

    updates eslintrc

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,24 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
+import Vue from 'vue';
+import VueRouter from 'vue-router';
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: "/",
-    name: "home",
-    component: () => import(/* webpackChunkName: "about" */ "../pages/Home.vue")
+    path: '/',
+    name: 'home',
+    component: () => import(/* webpackChunkName: "about" */ '../pages/Home.vue')
   },
   {
-    path: "/experience",
-    name: "experience",
+    path: '/experience',
+    name: 'experience',
     component: () =>
-      import(/* webpackChunkName: "about" */ "../pages/Experience.vue")
+      import(/* webpackChunkName: "about" */ '../pages/Experience.vue')
   }
 ];
 
 const router = new VueRouter({
-  mode: "history",
+  mode: 'history',
   base: process.env.BASE_URL,
   routes
 });
