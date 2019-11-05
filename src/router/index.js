5267581e31279c2ae1049905f9ae3ccbb4c51fd7
1forh <1forhlol@gmail.com>
2019-11-05 18:21:58 -0500

    removes unecessary code

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
 
 Vue.use(VueRouter);
 
@@ -8,16 +7,13 @@ const routes = [
   {
     path: "/",
     name: "home",
-    component: Home
+    component: () => import(/* webpackChunkName: "about" */ "../pages/Home.vue")
   },
   {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
+    path: "/experience",
+    name: "experience",
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+      import(/* webpackChunkName: "about" */ "../pages/Experience.vue")
   }
 ];
 
