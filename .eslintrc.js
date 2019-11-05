4733efaeaf26f165723abe67c42a4637149ea6db
1forh <1forhlol@gmail.com>
2019-11-05 18:29:36 -0500

    wip

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,8 @@ module.exports = {
         singleQuote: true,
         semi: true,
         trailingComma: 'es5',
+        arrowParens: 'always',
+        printWidth: 200,
       },
     ],
   },
--- src/router/index.js
+++ src/router/index.js
@@ -7,20 +7,19 @@ const routes = [
   {
     path: '/',
     name: 'home',
-    component: () => import(/* webpackChunkName: "about" */ '../pages/Home.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../pages/Home.vue'),
   },
   {
     path: '/experience',
     name: 'experience',
-    component: () =>
-      import(/* webpackChunkName: "about" */ '../pages/Experience.vue')
-  }
+    component: () => import(/* webpackChunkName: "about" */ '../pages/Experience.vue'),
+  },
 ];
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes
+  routes,
 });
 
 export default router;
