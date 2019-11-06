871d3458c2e6659930f74b24237452fe33080714
1forh <1forhlol@gmail.com>
2019-11-05 20:46:03 -0500

    wip

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,20 +3,13 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: ['plugin:vue/essential', '@vue/prettier'],
+  extends: [
+    'plugin:vue/essential',
+    '@vue/airbnb',
+  ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'prettier/prettier': [
-      'warn',
-      {
-        singleQuote: true,
-        semi: true,
-        trailingComma: 'es5',
-        arrowParens: 'always',
-        printWidth: 200,
-      },
-    ],
   },
   parserOptions: {
     parser: 'babel-eslint',
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import Home from '../views/Home.vue';
 
 Vue.use(VueRouter);
 
@@ -7,12 +8,15 @@ const routes = [
   {
     path: '/',
     name: 'home',
-    component: () => import(/* webpackChunkName: "about" */ '../pages/Home.vue'),
+    component: Home,
   },
   {
-    path: '/experience',
-    name: 'experience',
-    component: () => import(/* webpackChunkName: "about" */ '../pages/Experience.vue'),
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
   },
 ];
 
