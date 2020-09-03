a1d24278643ebff79b9615c7911de18affde4cee
1forh <1forhlol@gmail.com>
2020-09-02 23:19:45 -0400

    updates purge whitelist

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,26 @@
+const mdx = require('@mdx-js/mdx');
+
 module.exports = {
   purge: {
     mode: 'all',
-    content: ['./src/**/*.js'],
+    content: ['./src/**/*.{js,mdx}'],
     options: {
-      whitelist: ['h1', 'h2', 'h3', 'p', 'blockquote', 'strong'],
+      extractors: [
+        {
+          extensions: ['mdx'],
+          extractor: (content) => {
+            content = mdx.sync(content);
+
+            // Capture as liberally as possible, including things like `h-(screen-1.5)`
+            const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || [];
+
+            // Capture classes within other delimiters like .block(class="w-1/2") in Pug
+            const innerMatches = content.match(/[^<>"'`\s.(){}[\]#=%]*[^<>"'`\s.(){}[\]#=%:]/g) || [];
+
+            return broadMatches.concat(innerMatches);
+          },
+        },
+      ],
     },
   },
   theme: {
