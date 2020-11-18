0ca087612232f17de92ca6b9b3e26b18736e4a5e
1forh <1forhlol@gmail.com>
2020-11-18 13:04:37 -0500

    🔧 updates nuxt.config with meta properties

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,28 +1,41 @@
+const CONFIG = {
+  site_url: 'https://www.zacharyminner.com',
+  site_name: 'Zachary Minner',
+  short_name: 'Zach Minner',
+  description: 'Projects, articles and other random stuff by Zach Minner.',
+  theme_color: '#ffffff',
+};
+
 export default {
   // Target (https://go.nuxtjs.dev/config-target)
   target: 'static',
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'zacharyminner.com',
+    title: CONFIG.site_name,
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'viewport', name: 'viewport', content: 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0, minimal-ui' },
+      { hid: 'og:title', name: 'og:title', content: CONFIG.site_name },
+      { hid: 'og:site_name', name: 'og:site_name', content: CONFIG.site_name },
+      { hid: 'description', name: 'description', content: CONFIG.description },
+      { hid: 'og:description', name: 'og:description', content: CONFIG.description },
+      { hid: 'theme-color', name: 'theme-color', content: CONFIG.theme_color },
+      { hid: 'apple-mobile-web-app-capable', name: 'apple-mobile-web-app-capable', content: 'yes' },
+      { hid: 'apple-mobile-web-app-status-bar-style', name: 'apple-mobile-web-app-status-bar-style', content: 'white' },
+      { hid: 'apple-mobile-web-app-title', name: 'apple-mobile-web-app-title', content: CONFIG.site_name },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'canonical', href: `${CONFIG.site_url}/` },
+    ],
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: [
-  ],
+  css: [],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [
-    '@/plugins/vue-tailwind/index.js'
-  ],
+  plugins: ['@/plugins/vue-tailwind/index.js'],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
@@ -50,6 +63,21 @@ export default {
   content: {},
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
-  build: {
-  }
-}
+  build: {},
+
+  pwa: {
+    meta: false,
+    workbox: {
+      offlineAnalytics: true,
+    },
+    manifest: {
+      name: CONFIG.site_name,
+      short_name: CONFIG.site_name,
+      description: CONFIG.description,
+      theme_color: CONFIG.theme_color,
+      background_color: CONFIG.theme_color,
+      lang: 'en',
+      display: 'standalone',
+    },
+  },
+};
