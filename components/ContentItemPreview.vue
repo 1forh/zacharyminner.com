70ce8a3e3ad594fe27e7133a8a5c33983f70d045
1forh <1forhlol@gmail.com>
2020-11-18 13:04:24 -0500

    🐛 fixes issue with nuxt-link in ssr

--- components/ContentItemPreview.vue
+++ components/ContentItemPreview.vue
@@ -1,5 +1,5 @@
 <template>
-  <nuxt-link :to="item.path" class="space-y-2">
+  <a :href="item.path" class="space-y-2">
     <div>
       <h3 class="text-20 font-semibold">{{ item.title }}</h3>
       <p v-if="formattedDate" class="text-gray-500">{{ formattedDate }}</p>
@@ -8,7 +8,7 @@
     <div v-if="item.tags" class="flex space-x-3">
       <div v-for="(tag, index) in item.tags" :key="index" class="bg-primary-200 text-primary-800 rounded-sm px-2 py-1 text-12 font-semibold">{{ tag }}</div>
     </div>
-  </nuxt-link>
+  </a>
 </template>
 
 <script>
