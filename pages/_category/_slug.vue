23dc646a7410bd5fb78e8e899e4568c749e63ef0
1forh <1forhlol@gmail.com>
2020-11-22 11:43:09 -0500

    🚧 wip

--- pages/_category/_slug.vue
+++ pages/_category/_slug.vue
@@ -10,6 +10,9 @@
         <h1 class="mt-2 block text-3xl text-center leading-8 font-bold tracking-tight text-gray-900 sm:text-4xl" v-balance-text>
           {{ article.title }}
         </h1>
+        <div class="flex justify-center mb-8 mt-4" v-if="article.website">
+          <a :href="article.website" target="_blank" class="text-sm font-bold border-2 border-gray-800 text-gray-800 px-3 py-1 hover:bg-gray-800 hover:text-white">View website</a>
+        </div>
       </header>
       <div class="mt-6 prose prose-indigo prose-lg text-gray-500 mx-auto">
         <nuxt-content :document="article" />
