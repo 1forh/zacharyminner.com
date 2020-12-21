170efd56e6ace8f6580299acf3989a774948fbae
1forh <1forhlol@gmail.com>
2020-12-20 20:27:21 -0500

    🚧 starts working on BaseCode component

--- components/global/BaseCode.vue
+++ components/global/BaseCode.vue
@@ -1,10 +1,21 @@
 <template>
-  <code><slot /></code>
+  <pre :clas="[languageClass]"><code><slot /></code></pre>
 </template>
 
 <script>
 export default {
   name: 'BaseCode',
+  props: {
+    lang: {
+      type: String,
+      default: 'javascript',
+    },
+  },
+  computed: {
+    languageClass() {
+      return `language-${this.lang}`;
+    },
+  },
 };
 </script>
 
