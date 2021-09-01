02a8f85fe4e48df79356876465688bd4be4374bf
1forh <1forhlol@gmail.com>
2021-08-31 23:36:06 -0400

    🍱 updates assets

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,7 +1,7 @@
 const siteMetadata = {
   title: 'Zachary Minner - Full Stack Developer',
   author: 'Zachary Minner',
-  headerTitle: 'Zachary Minner',
+  headerTitle: "Zac's Blog",
   description: 'Projects, articles and other random stuff by Zachary Minner.',
   language: 'en-us',
   siteUrl: 'https://www.zacharyminner.com',
