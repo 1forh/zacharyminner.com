391f945e953aa14d1d0f5fd9b352e2f61fbc3796
1forh <1forhlol@gmail.com>
2021-08-31 23:57:14 -0400

    🐛 fixes url

--- layouts/ProjectLayout.js
+++ layouts/ProjectLayout.js
@@ -7,7 +7,7 @@ import Tag from '@/components/Tag'
 import siteMetadata from '@/data/siteMetadata'
 import Comments from '@/components/comments'
 
-const editUrl = (fileName) => `${siteMetadata.siteRepo}/blob/master/data/blog/${fileName}`
+const editUrl = (fileName) => `${siteMetadata.siteRepo}/blob/master/data/projects/${fileName}`
 const discussUrl = (slug) =>
   `https://mobile.twitter.com/search?q=${encodeURIComponent(
     `${siteMetadata.siteUrl}/projects/${slug}`
