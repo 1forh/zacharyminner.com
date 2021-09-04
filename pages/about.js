228e39b45615b643e4ce4c71c7456a342ae740bf
1forh <1forhlol@gmail.com>
2021-09-04 17:39:01 -0400

    ✨ starts api; updates about page

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,20 @@
 import { MDXLayoutRenderer } from '@/components/MDXComponents'
 import { getFileBySlug } from '@/lib/mdx'
+import axios from 'axios'
+import useSWR from 'swr'
 
 const DEFAULT_LAYOUT = 'AuthorLayout'
+
+const fetcher = (query) =>
+  fetch('/api/graphql', {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json',
+    },
+    body: JSON.stringify({ query }),
+  })
+    .then((res) => res.json())
+    .then((json) => json.data)
 
 export async function getStaticProps() {
   const authorDetails = await getFileBySlug('authors', ['default'])
@@ -10,6 +23,8 @@ export async function getStaticProps() {
 
 export default function About({ authorDetails }) {
   const { mdxSource, frontMatter } = authorDetails
+  const { data, error } = useSWR('{ users { name } }', fetcher)
+  console.log({ data, error })
 
   return (
     <MDXLayoutRenderer
