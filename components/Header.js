2af321a3a2bc670b19e81f55e0fa3d850ef98060
1forh <1forhlol@gmail.com>
2021-09-04 18:32:44 -0400

    ♻️ creates Header components

---
+++ components/Header.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import siteMetadata from '@/data/siteMetadata'
+import headerNavLinks from '@/data/headerNavLinks'
+import Logo from '@/data/logo.svg'
+import Link from './Link'
+import MobileNav from './MobileNav'
+import ThemeSwitch from './ThemeSwitch'
+
+export default function Header() {
+  return (
+    <header className="flex items-center justify-between py-10">
+      <div>
+        <Link href="/" aria-label="Zac's Garden">
+          <div className="flex items-center justify-between">
+            <div className="mr-3">
+              <Logo />
+            </div>
+            {typeof siteMetadata.headerTitle === 'string' ? (
+              <div className="h-8 text-2xl font-semibold">{siteMetadata.headerTitle}</div>
+            ) : (
+              siteMetadata.headerTitle
+            )}
+          </div>
+        </Link>
+      </div>
+      <div className="flex items-center text-base leading-5">
+        <div className="hidden sm:block">
+          {headerNavLinks.map((link) => (
+            <Link
+              key={link.title}
+              href={link.href}
+              className="p-1 font-medium text-gray-900 sm:p-4 dark:text-gray-100"
+            >
+              {link.title}
+            </Link>
+          ))}
+        </div>
+        <ThemeSwitch />
+        <MobileNav />
+      </div>
+    </header>
+  )
+}
--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -1,47 +1,12 @@
-import siteMetadata from '@/data/siteMetadata'
-import headerNavLinks from '@/data/headerNavLinks'
-import Logo from '@/data/logo.svg'
-import Link from './Link'
 import SectionContainer from './SectionContainer'
 import Footer from './Footer'
-import MobileNav from './MobileNav'
-import ThemeSwitch from './ThemeSwitch'
+import Header from './Header'
 
 const LayoutWrapper = ({ children }) => {
   return (
     <SectionContainer>
       <div className="flex flex-col justify-between h-screen">
-        <header className="flex items-center justify-between py-10">
-          <div>
-            <Link href="/" aria-label="Zac's Garden">
-              <div className="flex items-center justify-between">
-                <div className="mr-3">
-                  <Logo />
-                </div>
-                {typeof siteMetadata.headerTitle === 'string' ? (
-                  <div className="h-8 text-2xl font-semibold">{siteMetadata.headerTitle}</div>
-                ) : (
-                  siteMetadata.headerTitle
-                )}
-              </div>
-            </Link>
-          </div>
-          <div className="flex items-center text-base leading-5">
-            <div className="hidden sm:block">
-              {headerNavLinks.map((link) => (
-                <Link
-                  key={link.title}
-                  href={link.href}
-                  className="p-1 font-medium text-gray-900 sm:p-4 dark:text-gray-100"
-                >
-                  {link.title}
-                </Link>
-              ))}
-            </div>
-            <ThemeSwitch />
-            <MobileNav />
-          </div>
-        </header>
+        <Header />
         <main className="mb-auto">{children}</main>
         <Footer />
       </div>
