7d2812eefb677ca3b6d196dcb8d217b55a86df4c
1forh <1forhlol@gmail.com>
2021-09-04 21:12:57 -0400

    ♻️ automatically loads comments

--- components/comments/Giscus.js
+++ components/comments/Giscus.js
@@ -39,6 +39,11 @@ const Giscus = ({ mapping }) => {
     }
   }, [commentsTheme, mapping])
 
+  useEffect(() => {
+    LoadComments()
+    console.log('hit')
+  }, [])
+
   // Reload on theme change
   useEffect(() => {
     const iframe = document.querySelector('iframe.giscus-frame')
@@ -48,7 +53,7 @@ const Giscus = ({ mapping }) => {
 
   return (
     <div className="pt-6 pb-6 text-center text-gray-700 dark:text-gray-300">
-      {enableLoadComments && <button onClick={LoadComments}>Load Comments</button>}
+      {/* {enableLoadComments && <button onClick={LoadComments}>Load Comments</button>} */}
       <div className="giscus" id={COMMENTS_ID} />
     </div>
   )
