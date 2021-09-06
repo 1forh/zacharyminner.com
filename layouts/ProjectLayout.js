3606656b17e229b04f7d6d448d5e515e003c12da
1forh <1forhlol@gmail.com>
2021-09-05 22:15:50 -0400

    💄 updates styles

--- layouts/ProjectLayout.js
+++ layouts/ProjectLayout.js
@@ -19,7 +19,7 @@ const discussUrl = (slug) =>
 const postDateTemplate = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }
 
 export default function PostLayout({ frontMatter, authorDetails, next, prev, children }) {
-  const { slug, fileName, date, title, tags, website, appStoreUrl } = frontMatter
+  const { slug, fileName, title, tags, website, appStoreUrl } = frontMatter
 
   return (
     <SectionContainer>
@@ -30,22 +30,8 @@ export default function PostLayout({ frontMatter, authorDetails, next, prev, chi
       />
       <article>
         <div className="xl:divide-y xl:divide-gray-200 xl:dark:divide-gray-700">
-          <header className="pt-6 xl:pb-6">
-            <div className="space-y-1 text-center">
-              <dl className="space-y-10">
-                <div>
-                  <dt className="sr-only">Published on</dt>
-                  <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
-                    <time dateTime={date}>
-                      {new Date(date).toLocaleDateString(siteMetadata.locale, postDateTemplate)}
-                    </time>
-                  </dd>
-                </div>
-              </dl>
-              <div>
-                <PageTitle>{title}</PageTitle>
-              </div>
-            </div>
+          <header className="pt-6 text-center xl:pb-6">
+            <PageTitle>{title}</PageTitle>
           </header>
           <div
             className="pb-8 divide-y divide-gray-200 xl:divide-y-0 dark:divide-gray-700 xl:grid xl:grid-cols-4 xl:gap-x-6"
@@ -64,7 +50,7 @@ export default function PostLayout({ frontMatter, authorDetails, next, prev, chi
               )}
 
               {website && (
-                <SidebarWidget className="pt-0 lg:pt-6">
+                <SidebarWidget className={`lg:pt-6 ${appStoreUrl ? '!pt-0' : '!pt-6'}`}>
                   <div className="flex justify-center lg:justify-start">
                     <a
                       href={website}
