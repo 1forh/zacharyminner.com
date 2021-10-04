fb3952d6cb4db8a857885fe7ff5dfbd96b65473e
Zac <1forhlol@gmail.com>
2021-10-04 10:50:45 -0400

    updates snippets sorting logic

--- pages/snippets.js
+++ pages/snippets.js
@@ -4,7 +4,14 @@ import SnippetListLayout from '@/layouts/SnippetListLayout'
 import { PageSEO } from '@/components/SEO'
 
 export async function getStaticProps() {
-  const snippets = await getAllFilesFrontMatter('snippets')
+  let snippets = await getAllFilesFrontMatter('snippets')
+  snippets = snippets.sort((a, b) => {
+    if (a.weight > b.weight) {
+      return 1
+    } else {
+      return -1
+    }
+  })
   return { props: { snippets } }
 }
 
